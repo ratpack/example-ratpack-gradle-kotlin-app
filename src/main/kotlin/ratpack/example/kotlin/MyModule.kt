9d4519d64accc26a2380f8b549be7ef34b206c93
jamming <juanjoaguililla@gmail.com>
2016-04-05 11:07:32 +0200

    Inline LoggingHandler as a Kotlin SAM lambda

--- src/main/kotlin/ratpack/example/kotlin/MyModule.kt
+++ src/main/kotlin/ratpack/example/kotlin/MyModule.kt
@@ -8,18 +8,21 @@ import ratpack.handling.HandlerDecorator
  * An example Guice module.
  */
 class MyModule : AbstractModule() {
-
-  /**
-   * Adds a service impl to the application, and registers a decorator so that all requests are logged.
-   * Registered implementations of {@link ratpack.handling.HandlerDecorator} are able to decorate the application handler.
-   *
-   * @see MyHandler
-   */
-  protected override fun configure() {
-    bind(MyService::class.java).to(MyServiceImpl::class.java)
-    bind(MyHandler::class.java)
-    Multibinder.newSetBinder(binder(), HandlerDecorator::class.java)
-      .addBinding()
-      .toInstance(HandlerDecorator.prepend(LoggingHandler()))
-  }
+    /**
+     * Adds a service impl to the application, and registers a decorator so that all requests are logged.
+     * Registered implementations of {@link ratpack.handling.HandlerDecorator} are able to decorate the
+     * application handler.
+     *
+     * @see MyHandler
+     */
+    override fun configure() {
+        bind(MyService::class.java).to(MyServiceImpl::class.java)
+        bind(MyHandler::class.java)
+        Multibinder.newSetBinder(binder(), HandlerDecorator::class.java)
+            .addBinding()
+            .toInstance(HandlerDecorator.prepend {
+                println("Received: ${it.request.uri}")
+                it.next()
+            })
+    }
 }
