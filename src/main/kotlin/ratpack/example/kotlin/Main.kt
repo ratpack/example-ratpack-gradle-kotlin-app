8d12358c5f5db9a1cd68cd26e35b867cb3877927
jamming <juanjoaguililla@gmail.com>
2016-04-05 11:13:50 +0200

    Use helper methods in the example

--- src/main/kotlin/ratpack/example/kotlin/Main.kt
+++ src/main/kotlin/ratpack/example/kotlin/Main.kt
@@ -1,61 +1,57 @@
 package ratpack.example.kotlin
 
 import org.slf4j.LoggerFactory.getLogger
-import ratpack.guice.Guice
 import ratpack.handling.Context
 import ratpack.server.BaseDir
-import ratpack.server.RatpackServer
 
-object Main {
-  private val log = getLogger(Main::class.java)
+object Main // Only a tag for the logger
+private val log = getLogger(Main::class.java)
 
-  @JvmStatic
-  fun main(args: Array<String>) {
+fun main(args: Array<String>) {
     try {
-      RatpackServer.of { server ->
-        server
-          .serverConfig { config ->
-            config.baseDir(BaseDir.find())
-          }
-          .registry(Guice.registry { bindingSpec ->
-            bindingSpec.module(MyModule())
-          })
-          .handlers { chain ->
-            chain
-              // Map to /foo
-              .path("foo", { context ->
-                context.render("from the foo handler")
-              })
-              // Map to /bar
-              .path("bar", { context ->
-                context.render("from the bar handler")
-              })
-              // Map to /baz using a Kotlin function
-              .path("baz", ::bazHandler)
-              // Set up a nested routing block, which is delegated to `nestedHandler`
-              .prefix("nested", { nested ->
-                nested
-                  .path(":var1/:var2?", { context ->
-                    // The path tokens are the :var1 and :var2 path components above
-                    val pathTokens = context.pathTokens
-                    context
-                      .render("from the nested handler, var1: ${pathTokens["var1"]}, var2: ${pathTokens["var2"]}")
-                  })
-              })
-              // Map to a dependency injected handler
-              .path("injected", MyHandler::class.java)
-              // Bind the /static app path to the src/ratpack/assets/images dir
-              .prefix("static", { nested ->
-                nested.fileSystem("assets/images", { f -> f.files() })
-              })
-              .all { context -> context.render("root handler!") }
-          }
-      }.start()
-    } catch (e: Exception) {
-      log.error("", e)
-      System.exit(1)
+        createServer().start()
     }
-  }
+    catch (e: Exception) {
+        log.error("", e)
+        System.exit(1)
+    }
 }
 
+fun createServer() = kserverOf {
+    kserverConfig {
+        baseDir(BaseDir.find())
+    }
+
+    kguiceRegistry {
+        module(MyModule())
+    }
+
+    khandlers {
+        kpath("foo") { render("from the foo handler") }
+        kpath("bar") { render("from the bar handler") }
+
+        // Map to /baz using a Kotlin function
+        kpath("baz", ::bazHandler)
+
+        // Set up a nested routing block, which is delegated to `nestedHandler`
+        kprefix("nested") {
+            kpath(":var1/:var2?") {
+                // The path tokens are the :var1 and :var2 path components above
+                render("from the nested handler, var1: ${pathTokens["var1"]}, var2: ${pathTokens["var2"]}")
+            }
+        }
+
+        // Map to a dependency injected handler
+        path("injected", MyHandler::class.java)
+
+        // Bind the /static app path to the src/ratpack/assets/images dir
+        kprefix("static") {
+            kfileSystem("assets/images") { files() }
+        }
+
+        kall { render("root handler!") }
+    }
+}
+
+/** A handler as a method */
 fun bazHandler(context: Context) = context.render("from the baz handler")
--- src/test/kotlin/ratpack/example/kotlin/SiteTest.kt
+++ src/test/kotlin/ratpack/example/kotlin/SiteTest.kt
@@ -1,67 +1,54 @@
 package ratpack.example.kotlin
 
-import com.google.common.io.CharStreams
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
-import ratpack.test.MainClassApplicationUnderTest
-import java.io.InputStreamReader
-import kotlin.test.assertEquals
-import kotlin.test.fail
 
 @RunWith(JUnit4::class)
 class SiteTest {
+    val app = kappFromServer(createServer ())
 
-  val aut = MainClassApplicationUnderTest(Main::class.java)
-
-  @Test fun fooHandler() {
-    assertEquals("from the foo handler", get("/foo"))
-  }
-
-  @Test fun barHandler() {
-    assertEquals("from the bar handler", get("/bar"))
-  }
-
-  @Test fun bazHandler() {
-    assertEquals("from the baz handler", get("/baz"))
-  }
-
-  @Test fun nestedHandler() {
-    assertEquals("from the nested handler, var1: x, var2: null",
-                 get("/nested/x"))
-    assertEquals("from the nested handler, var1: x, var2: y",
-                 get("/nested/x/y"))
-  }
-
-  @Test fun injectedHandler() {
-    assertEquals("service value: service-value", get("/injected"))
-  }
-
-  @Test fun staticHandler() {
-    assertEquals("text asset\n", get("/static/test.txt"))
-  }
-
-  @Test fun rootHandler() {
-    assertEquals("root handler!", get("/"))
-    assertEquals("root handler!", get("/unknown-path"))
-  }
-
-  private fun get(path: String): String {
-    val uri = aut.address.resolve(path)
-    val stream = uri.toURL().openStream()
-    try {
-      val reader = InputStreamReader(stream)
-      try {
-        return CharStreams.toString(reader)
-      } finally {
-        reader.close()
-      }
-    } catch (ex: Exception) {
-      fail(ex.toString())
-      return "" // unreachable
-    } finally {
-      stream?.close()
+    @Test fun fooHandler() {
+        app.ktest {
+            assert("from the foo handler" == getBody("/foo"))
+        }
     }
-  }
 
+    @Test fun barHandler() {
+        app.ktest {
+            assert("from the bar handler" == getBody("/bar"))
+        }
+    }
+
+    @Test fun bazHandler() {
+        app.ktest {
+            assert("from the baz handler" == getBody("/baz"))
+        }
+    }
+
+    @Test fun nestedHandler() {
+        app.ktest {
+            assert("from the nested handler, var1: x, var2: null" == getBody("/nested/x"))
+            assert("from the nested handler, var1: x, var2: y" == getBody("/nested/x/y"))
+        }
+    }
+
+    @Test fun injectedHandler() {
+        app.ktest {
+            assert("service value: service-value" == getBody("/injected"))
+        }
+    }
+
+    @Test fun staticHandler() {
+        app.ktest {
+            assert("text asset\n" == getBody("/static/test.txt"))
+        }
+    }
+
+    @Test fun rootHandler() {
+        app.ktest {
+            assert("root handler!" == getBody("/"))
+            assert("root handler!" == getBody("/unknown-path"))
+        }
+    }
 }
